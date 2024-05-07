e156fa82d93b33c480047ad3ad4600ba07c37092
gbartelli <fushayprevate@gmail.com>
2024-05-07 11:50:17 -0300

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,59 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getUrl } from "./lib/get-url";
+import { NextRequest } from "next/server";
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from "@/routes";
+import NextAuth, { Session } from "next-auth";
 
-export function middleware(request: NextRequest) {
-  // Determinar o prefixo do cookie com base no ambiente
-  const cookiePrefix = process.env.NODE_ENV === "production" ? "__Secure-" : "";
-  console.log(process.env.NODE_ENV);
+import authConfig from "./auth.config";
 
-  // Construir o nome do cookie usando o prefixo determinado
-  const cookieName = "{cookiePrefix}authjs.session-token";
+const { auth: middleware } = NextAuth(authConfig);
 
-  // Obter o token do cookie usando o nome do cookie construído
-  const token = request.cookies.get(cookieName);
-  const pathname = request.nextUrl.pathname;
+export default middleware(
+  (req: NextRequest & { auth: Session | null }): Response | void => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
 
-  if (pathname === "/auth" && token) {
-    return NextResponse.redirect(new URL(getUrl("/app")));
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.some((route) => {
+      if (route === "/") {
+        return nextUrl.pathname === route;
+      } else {
+        return nextUrl.pathname.startsWith(route);
+      }
+    });
+
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isApiAuthRoute) return;
+
+    if (isAuthRoute) {
+      if (isLoggedIn) {
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+      }
+      return;
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+      let callbackUrl = nextUrl.pathname;
+      if (nextUrl.search) {
+        callbackUrl += nextUrl.search;
+      }
+
+      const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+      return Response.redirect(
+        new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+      );
+    }
+
+    return;
   }
+);
 
-  if (pathname.includes("/app") && !token) {
-    return NextResponse.redirect(new URL(getUrl("/auth")));
-  }
-}
-
+// Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next|api).*)", "/"],
 };
