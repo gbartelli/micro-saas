f4d31fcda8b8ab18bcf4530aa037fe7feaa13f5b
gbartelli <fushayprevate@gmail.com>
2024-05-07 11:16:16 -0300

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,17 @@
-/* import { NextRequest, NextResponse } from 'next/server'
-import { getUrl } from './lib/get-url'
-
-export function middleware(request: NextRequest) {
-    const token = request.cookies.get('authjs.session-token')
-    const pathname = request.nextUrl.pathname
-
-    if (pathname === '/auth' && token) {
-        return NextResponse.redirect(new URL(getUrl('/app')))
-    }
-
-    if (pathname.includes('/app') && !token) {
-        return NextResponse.redirect(new URL(getUrl('/auth')))
-    }
-}
-
-export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-}
- */
 import { NextRequest, NextResponse } from "next/server";
 import { getUrl } from "./lib/get-url";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("__secure-authjs.session-token");
-  const pathname = request.nextUrl.pathname;
+  // Determinar o prefixo do cookie com base no ambiente
+  const cookiePrefix = process.env.NODE_ENV === "production" ? "__Secure-" : "";
+  console.log(process.env.NODE_ENV);
 
-  /*   console.log({
-    token: token?.value,
-    pathname,
-  }); */
+  // Construir o nome do cookie usando o prefixo determinado
+  const cookieName = "${cookiePrefix}authjs.session-token";
+
+  // Obter o token do cookie usando o nome do cookie construído
+  const token = request.cookies.get(cookieName);
+  const pathname = request.nextUrl.pathname;
 
   if (pathname === "/auth" && token) {
     return NextResponse.redirect(new URL(getUrl("/app")));
@@ -40,5 +23,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next).*)(.+)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
